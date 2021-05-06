82ff1d222e87d4e14345c21865bbe026fa04be9f
vovks <sveta.v.fcb@gmail.com>
2021-05-06 20:34:07 +0300

    commit all

--- src/test/java/HomeworkSelenium.java
+++ src/test/java/HomeworkSelenium.java
@@ -2,17 +2,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class HomeworkSelenium {
